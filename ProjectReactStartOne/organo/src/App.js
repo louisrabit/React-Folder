913cd883f356fe2221ffa4086a36509fd8ea473b
Luis Manuel Brito Coelho <98318766+louisrabit@users.noreply.github.com>
2024-10-11 00:40:31 +0100

    TeamsFieldsFormColorsPrimSecond

--- ProjectReactStartOne/organo/src/App.js
+++ ProjectReactStartOne/organo/src/App.js
@@ -2,9 +2,43 @@
 import { useState } from 'react';
 import Banner from './component/Banner/banner';
 import Form from './component/Form/Form';
-import Time from './component/Time/index';
+import Time from './component/Time';
 
 function App() {
+
+  const teams = [
+    {
+      name: 'Front-End',
+      corPrimaria: '#82CFFA',
+      corSecundaria: '#E8F8FF',
+  },
+  {
+    name: 'Data Sciense',
+      corPrimaria: '#A6D157',
+      corSecundaria: '#F0F8E2',
+  },
+  {
+    name: 'Devops',
+      corPrimaria: '#E06B69',
+      corSecundaria: '#FDE7E8',
+  },
+  {
+      name: 'UX e Design',
+      corPrimaria: '#D86EBF',
+      corSecundaria: '#FAE5F5',
+  },
+  {
+    name: 'Mobile',
+      corPrimaria: '#FEBA05',
+      corSecundaria: '#FFF5D9',
+  },
+  {
+    name: 'Inovação e Gestão',
+      corPrimaria: '#FF8A29',
+      corSecundaria: '#FFEEDF',
+  }
+  ]
+
 
 const [persons, setPersons] = useState([])
 
@@ -18,9 +52,13 @@ const newPersonObject = (personObject) => {
       <Banner />
       
       <Form objectData = {personObject => newPersonObject(personObject)} />
+      {teams.map(teamUser =>
+       <Time key={teamUser.name}
+       name={teamUser.name} 
+       corPrimaria={teamUser.corPrimaria}
+       corSecundaria={teamUser.corSecundaria}
+       /> )}
    
-   <Time name="Programming"/>
-   <Time name="Data Science"/>
     </div>
   );
 }
--- ProjectReactStartOne/organo/src/component/Time/index.js
+++ ProjectReactStartOne/organo/src/component/Time/index.js
@@ -2,11 +2,18 @@ import './Time.css';
 
 
 const Time = (props) => {
+
+    const secndaryColor = {backgroundColor: props.corSecundaria};
+     const primaryColor = {borderColor: props.corPrimaria}
+
+
     return (
-        <section>
-        <h3 >
+        // <section className="time" style={{backgroundColor: props.corSecundaria}}>
+        <section className="time" style={secndaryColor}>
+        <h3 style={primaryColor}>
             {props.name}
         </h3>
         </section>
     )
 }
+export default Time;
