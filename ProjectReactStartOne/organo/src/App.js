393d63bb805a30cbd4ee62e48dfe980d0ee29d91
louisrabit <you@example.com>
2024-10-10 20:55:02 +0100

    teamComponent

--- ProjectReactStartOne/organo/src/App.js
+++ ProjectReactStartOne/organo/src/App.js
@@ -2,7 +2,7 @@
 import { useState } from 'react';
 import Banner from './component/Banner/banner';
 import Form from './component/Form/Form';
-
+import Time from './component/Time/index';
 
 function App() {
 
@@ -10,6 +10,7 @@ const [persons, setPersons] = useState([])
 
 const newPersonObject = (personObject) => {
   console.log(personObject);
+  setPersons([...persons, personObject])
 }
 
   return (
@@ -17,6 +18,9 @@ const newPersonObject = (personObject) => {
       <Banner />
       
       <Form objectData = {personObject => newPersonObject(personObject)} />
+   
+   <Time name="Programming"/>
+   <Time name="Data Science"/>
     </div>
   );
 }
---
+++ ProjectReactStartOne/organo/src/component/Time/index.js
@@ -0,0 +1,12 @@
+import './Time.css';
+
+
+const Time = (props) => {
+    return (
+        <section>
+        <h3 >
+            {props.name}
+        </h3>
+        </section>
+    )
+}
