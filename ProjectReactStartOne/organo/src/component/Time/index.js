07923680b6ea47e72a26fdc13856522c41e89484
louisrabit <you@example.com>
2024-11-05 22:38:33 +0000

    ShowTimeIfExistsEmployer

--- ProjectReactStartOne/organo/src/component/Time/index.js
+++ ProjectReactStartOne/organo/src/component/Time/index.js
@@ -9,26 +9,28 @@ const Time = (props) => {
 
 
     return (
+
+      props.persons.length > 0 ? (
         // <section className="time" style={{backgroundColor: props.corSecundaria}}>
         <section className="time" style={secndaryColor}>
-
-        <h3 style={primaryColor}>
-            {props.name}
-        </h3>
+               <h3 style={primaryColor}>
+                 {props.name}
+                </h3>
 
         <div className='employersTeam'>
-        {props.persons.map(personObject => 
-  <Employer 
-    key={personObject.name} 
-    name={personObject.name} 
-    position={personObject.position}
-    image={personObject.image}
-  />
-)}
+             {props.persons.map(personObject => 
+              <Employer 
+                 key={personObject.name} 
+                 name={personObject.name} 
+                 position={personObject.position}
+                 image={personObject.image}
+                />
+                   )}
         </div>
-      
-      
         </section>
+
+) : "" 
+
     )
 }
 export default Time;
